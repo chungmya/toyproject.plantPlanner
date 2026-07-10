54b1c8fcc096898b6bd2d5128815cf43bb79d92a
bluepark <bluepark1023@netive.co.kr>
2026-07-10 14:23:38 +0900

    [fix]머지 충돌 해결

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,29 @@
-<<<<<<< HEAD
 <script setup>
-import { RouterView, RouterLink, useRoute } from 'vue-router'
+import { RouterView, RouterLink, useRoute } from "vue-router";
 
-const route = useRoute()
-=======
+const route = useRoute();
+</script>
+
 <template>
-  <div id="app">
-    <RouterView />
-
+  <div class="app-wrap">
     <nav class="tab-bar">
       <RouterLink to="/" class="tab-bar__item" :class="{ 'tab-bar__item--active': route.path === '/' }">
         <i class="ti ti-home" aria-hidden="true"></i>
         <span>홈</span>
       </RouterLink>
-
-      <RouterLink to="/plant/add" class="tab-bar__item" :class="{ 'tab-bar__item--active': route.path === '/plant/add' }">
-        <i class="ti ti-leaf" aria-hidden="true"></i>
-        <span>식물</span>
-      </RouterLink>
-
       <RouterLink to="/diary" class="tab-bar__item" :class="{ 'tab-bar__item--active': route.path === '/diary' }">
-        <i class="ti ti-notebook" aria-hidden="true"></i>
-        <span>타임라인</span>
-      </RouterLink>
-    </nav>
-  </div>
-</template>
-
-<script setup>
-import { RouterView, RouterLink, useRoute } from "vue-router";
-import "@/assets/scss/main.scss";
-
-const route = useRoute();
->>>>>>> 66a75131a119b231e392467273064a1507596847
-</script>
-
-<template>
-    <nav class="tab-bar">
-      <RouterLink
-        to="/"
-        class="tab-bar__item"
-        :class="{ 'tab-bar__item--active': route.path === '/' }"
-      >
-        <i class="ti ti-home" aria-hidden="true"></i>
-        <span>홈</span>
-      </RouterLink>
-
-      <RouterLink
-        to="/diary"
-        class="tab-bar__item"
-        :class="{ 'tab-bar__item--active': route.path === '/diary' }"
-      >
         <i class="ti ti-notebook" aria-hidden="true"></i>
         <span>일지</span>
       </RouterLink>
-
-      <RouterLink
-        to="/plant/list"
-        class="tab-bar__item"
-        :class="{ 'tab-bar__item--active': route.path === '/plant/list' }"
-      >
+      <RouterLink to="/plant/list" class="tab-bar__item" :class="{ 'tab-bar__item--active': route.path === '/plant/list' }">
         <i class="ti ti-leaf" aria-hidden="true"></i>
         <span>식물</span>
       </RouterLink>
-
-      <RouterLink
-        to="/favorite"
-        class="tab-bar__item"
-        :class="{ 'tab-bar__item--active': route.path === '/favorite' }"
-      >
+      <RouterLink to="/favorite" class="tab-bar__item" :class="{ 'tab-bar__item--active': route.path === '/favorite' }">
         <i class="ti ti-star" aria-hidden="true"></i>
         <span>초록별</span>
       </RouterLink>
     </nav>
-
     <RouterView />
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,40 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import { usePlantStore } from "@/stores/plant";
+import PlantCard from "@/components/plant/PlantCard.vue";
+import "@/assets/scss/pages/HomeView.scss";
 
+const plantStore = usePlantStore();
+
+function handleDelete(id) {
+  plantStore.removePlant(id);
+}
+</script>
 
 <template>
   <div class="home">
-  <header class="home__header">
-    <h1 class="home__title">식집사 다이어리</h1>
-    <div class="action-bar">
-      <RouterLink to="/plant/add" class="home__add-btn">
-        <i class="ti ti-circle-plus" aria-hidden="true"></i>
-      </RouterLink>
-      <RouterLink to="" class="home__alert-btn">
-        <i class="ti ti-bell home__bell" aria-hidden="true"></i>
-      </RouterLink>
+    <div class="home__banner">
+      <p class="home__banner-label">오늘의 식물 소개</p>
+      <div class="home__banner-top">
+        <h1 class="home__title">오.식.소</h1>
+        <RouterLink to="/plant/add" class="home__add-btn">
+          <i class="ti ti-circle-plus" aria-hidden="true"></i>
+        </RouterLink>
+      </div>
+      <div class="home__banner-img">
+        <span>배너 이미지 영역</span>
+      </div>
     </div>
-  </header>
 
-  <ul class="plant-list">
-    <PlantCard 
-    v-for="plant in plantStore.plants" 
-    :key="plant.id"
-    :id="plant.id"
-    :name="plant.name"
-    :value="plant.species"
-    :status="plant.status"
-    @delete="handleDelete"
-    />
-  </ul>
-</div>
+    <div class="home__content">
+      <div class="home__search">
+        <i class="ti ti-search" aria-hidden="true"></i>
+        <span>식물 검색</span>
+      </div>
+
+      <ul class="plant-list">
+        <PlantCard v-for="plant in plantStore.plants" :key="plant.id" :id="plant.id" :name="plant.name" :value="plant.species" :status="plant.status" @delete="handleDelete" />
+      </ul>
+    </div>
+  </div>
 </template>
-
-<script setup>
-import { RouterLink } from 'vue-router'
-import { usePlantStore } from '@/stores/plant'
-import PlantCard from '@/components/plant/PlantCard.vue'
-import "@/assets/scss/pages/HomeView.scss";
-
-const plantStore = usePlantStore()
-
-<<<<<<< HEAD
-=======
-//delete버튼
->>>>>>> 66a75131a119b231e392467273064a1507596847
-function handleDelete(id) {
-  plantStore.removePlant(id)
-}
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 66a75131a119b231e392467273064a1507596847
-const plants = [
-  { id: 1, value:'다육이', name:'하월시아', status:'safe' },
-  { id: 2, value:'아프리카식물', name:'아이오베숨', status:'warn' },
-  { id: 3, value:'관엽', name:'스킨답서스', status:'danger' },
-] 
-</script>
-
-
